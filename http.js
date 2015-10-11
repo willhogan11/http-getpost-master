4eea7fa9fd6a7b89b9d8b7a125e0498fd640ae67
Will Hogan <willhogan11@hotmail.com>
2015-10-11 01:40:37 +0100

    Full Version

---
+++ http.js
@@ -0,0 +1,18 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  console.log("REQUEST:");
+  console.log(request);
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello Will\n");
+  console.log("RESPONSE:");
+  console.log(response);
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
